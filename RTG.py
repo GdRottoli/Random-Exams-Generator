8a666c5438d34ab2e22c9cf9d2305e7f83ce87b0
Giovanni Daián Rottoli <gd.rottoli@gmail.com>
2020-04-19 18:46:16 -0300

    agregadas condiciones sobre ventana de agregar base

--- RTG.py
+++ RTG.py
@@ -3,6 +3,7 @@ from tkinter import *
 from tkinter import ttk
 
 from tkinter import filedialog, simpledialog, messagebox
+from tkinter.font import Font
 import yaml
 import os
 import generator
@@ -12,7 +13,7 @@ class Application():
     def __init__(self, master):
 
         self.master = master
-        self._topFrame = Frame(self.master, borderwidth=1)
+        self._topFrame = Frame(self.master, bd=1)
         self._topFrame.pack(fill=BOTH, expand=True)
         self._bottomFrame = Frame(self.master)
         self._bottomFrame.pack(side=BOTTOM, expand=True)
@@ -30,44 +31,44 @@ class Application():
             student_number_frame.pack(fill=X)
             lb_student_number = Label(student_number_frame, text="Tests:", width=6)
             lb_student_number.pack(side=LEFT, padx=5, pady=5)
-            self._entryStudentNumber = Entry(student_number_frame)
-            self._entryStudentNumber.pack(fill=X, padx=5, expand=True)
-            self._entryStudentNumber.insert(END, self.config["student_number"])
+            self._entry_student_number = Entry(student_number_frame)
+            self._entry_student_number.pack(fill=X, padx=5, expand=True)
+            self._entry_student_number.insert(END, self.config["student_number"])
 
             subject_frame = Frame(self._topFrame)
             subject_frame.pack(fill=X)
             lb_subject = Label(subject_frame, text="Subject:", width=6)
             lb_subject.pack(side=LEFT, padx=5, pady=5)
-            self._entrySubject = Entry(subject_frame)
-            self._entrySubject.pack(fill=X, padx=5, expand=True)
-            self._entrySubject.insert(END, self.config["subject"])
+            self._entry_subject = Entry(subject_frame)
+            self._entry_subject.pack(fill=X, padx=5, expand=True)
+            self._entry_subject.insert(END, self.config["subject"])
 
             topic_frame = Frame(self._topFrame)
             topic_frame.pack(fill=X)
             lb_topic = Label(topic_frame, text="Topic:", width=6)
             lb_topic.pack(side=LEFT, padx=5, pady=5)
-            self._entryTopic = Entry(topic_frame)
-            self._entryTopic.pack(fill=X, padx=5, expand=True)
-            self._entryTopic.insert(END, self.config["topic"])
+            self._entry_topic = Entry(topic_frame)
+            self._entry_topic.pack(fill=X, padx=5, expand=True)
+            self._entry_topic.insert(END, self.config["topic"])
 
             date_Frame = Frame(self._topFrame)
             date_Frame.pack(fill=X)
             lb_date = Label(date_Frame, text="Date:", width=6)
             lb_date.pack(side=LEFT, padx=5, pady=5)
-            self._entryDate = Entry(date_Frame)
-            self._entryDate.pack(fill=X, padx=5, expand=True)
-            self._entryDate.insert(END, self.config["date"])
+            self._entry_date = Entry(date_Frame)
+            self._entry_date.pack(fill=X, padx=5, expand=True)
+            self._entry_date.insert(END, self.config["date"])
 
             bases_title_frame = Frame(self._topFrame)
             bases_title_frame.pack(fill=X)
-            self._lbBases = Label(bases_title_frame, text="Problems Bases:", width=15)
-            self._lbBases.pack(side=LEFT, padx=5, pady=5)
+            lb_bases = Label(bases_title_frame, text="Problems Bases:", width=15)
+            lb_bases.pack(side=LEFT, padx=5, pady=5)
 
             bases_content_frame = Frame(self._topFrame)
             bases_content_frame.pack(fill=X)
-            self._listBases = Listbox(bases_content_frame)
-            self._listBases.pack(fill=X, padx=5, pady=5)
-            self.__updateList()
+            self._list_bases = Listbox(bases_content_frame)
+            self._list_bases.pack(fill=X, padx=5, pady=5)
+            self.__update_list()
 
         list_buttons_frame = Frame(self._topFrame)
         list_buttons_frame.pack(fill=X)
@@ -78,22 +79,22 @@ class Application():
 
         root.mainloop()
 
-    def __updateList(self):
-        self._listBases.delete(0, END)
+    def __update_list(self):
+        self._list_bases.delete(0, END)
         bases = self.config["question_bases"]
         questions_per_file = self.config["questions_per_file"]
         for i in range(len(bases)):
-            self._listBases.insert(END, "%s (%s)" % (bases[i], questions_per_file[i]))
+            self._list_bases.insert(END, "%s (%s)" % (bases[i], questions_per_file[i]))
 
     def __generation_event(self):
         if len(self.config["question_bases"]) == 0:
             messagebox.showerror("Error", "You must select at least one question base")
         else:
             try:
-                self.config["student_number"] = int(self._entryStudentNumber.get())
-                self.config["date"] = self._entryDate.get()
-                self.config["subject"] = self._entrySubject.get()
-                self.config["topic"] = self._entryTopic.get()
+                self.config["student_number"] = int(self._entry_student_number.get())
+                self.config["date"] = self._entry_date.get()
+                self.config["subject"] = self._entry_subject.get()
+                self.config["topic"] = self._entry_topic.get()
                 self.__save_config()
                 generator.file_generation()
             except ValueError:
@@ -106,18 +107,26 @@ class Application():
                                             initialdir=os.getcwd(),
                                             title="Please select a file:",
                                             filetypes=my_filetypes)
-        answer = simpledialog.askstring("Input", "Number of problems from this base?",
-                                                parent=self.master)
-        self.config["question_bases"].append(path)
-        self.config["questions_per_file"].append(int(answer))
-        self.__updateList()
+        if (len(path)>0):
+            try:
+                answer = int(simpledialog.askstring("Input", "Number of problems from this base?",
+                                                    parent=self.master))
+                self.config["question_bases"].append(path)
+                self.config["questions_per_file"].append(int(answer))
+                self.__update_list()
+            # manejar mejor estas excepciones...
+            except TypeError:   # si se cancela
+                pass
+            except ValueError:  # si vacio o no número, habría que llamar de nuevo la ventana
+                messagebox.showerror("Error", "The value was not a number, please try again.")
+
 
     def __remove_base(self):
         try:
-            selection = self._listBases.curselection()[0]
+            selection = self._list_bases.curselection()[0]
             del self.config["question_bases"][selection]
             del self.config["questions_per_file"][selection]
-            self.__updateList()
+            self.__update_list()
         except IndexError:
             pass
 
@@ -129,6 +138,7 @@ class Application():
 if __name__ == '__main__':
     root = Tk()
     root.title("Random Test Generator")
-    root.geometry("600x400")
+    root.geometry("400x400")
+    root.resizable(0, 0)
     app = Application(root)
     root.mainloop()
